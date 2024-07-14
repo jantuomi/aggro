e413572150995d886773cbc598dff1cd4d6b3aec
Jan Tuomi <jans.tuomi@gmail.com>
2024-07-14 10:49:17 +0300

    Add --progress plain to docker build

--- build-and-push.sh
+++ build-and-push.sh
@@ -3,5 +3,5 @@
 IMAGE=registry.jan.systems/aggro:latest
 
 set -euxo pipefail
-docker build --platform linux/amd64 -t $IMAGE .
+docker build --progress plain --platform linux/amd64 -t $IMAGE .
 docker push $IMAGE
